5706d128c40b267cb209ef5ae7684f63f613177d
EngAya <aya.ghoury@gmail.com>
2026-09-18 17:08:31 +0300

    chore: remove route:cache from deployment script

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,6 @@ php artisan route:clear
 php artisan config:cache
 php artisan event:cache
 php artisan view:cache
-php artisan route:cache
 
 # Restart queues (if using Supervisor for Laravel Horizon/Queue)
 # php artisan queue:restart
